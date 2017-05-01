65631f7ae7af20f2b541c7c630a6005fa2f7a96f
florent gallois <flgallois@gmail.com>
2017-05-01 16:13:20 +0200

    ajout de ft_list_foreach

---
+++ ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgallois <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/01 16:06:09 by fgallois          #+#    #+#             */
+/*   Updated: 2017/05/01 16:11:06 by fgallois         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	while (begin_list)
+	{
+		(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
